ed5f27fec48c48fe2f97f1588f43eb0448efc21f
CannonBowl-Kan <kanyanee.lekcham@nhn-playart.com>
2020-12-07 17:42:58 +0900

    Add Option in MainMenu

--- Beginners Circle 4/Assets/Scripts/Menu Scipts/MainMenu.cs
+++ Beginners Circle 4/Assets/Scripts/Menu Scipts/MainMenu.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
 public class MainMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public static float MouseSensitivity = 15f;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("BGVolume", Mathf.Log10(volume)*20);
+    }
+    public void SetMouseSensitivity(float sensitivity)
+    {
+        MouseSensitivity = sensitivity;
+    }
     public void QuitGame()
     {
         Application.Quit();
