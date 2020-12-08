c68f8aca1212bcbd356d3433879e013b31dc2e9c
YuiAzu <lekcham.kanyanee@gmail.com>
2020-12-08 10:47:10 +0900

    Add Timer
    Signed-off-by: CannonBowl-Kan <kanyanee.lekcham@nhn-playart.com>

---
+++ Beginners Circle 4/Assets/Scripts/GameSystem/Timer.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timeText;
+    private float oldseconds;
+    private float seconds;
+    private float minutes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldseconds = 0f;
+        seconds = 0f;
+        minutes = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds += Time.deltaTime;
+        if(seconds >= 60f)
+        {
+            minutes += 1;
+            seconds -= 60f;
+        }
+        if ((int)seconds != (int)oldseconds)
+        {
+            timeText.text = minutes.ToString("00")+ ":" + seconds.ToString("00");
+        }
+        oldseconds = seconds;
+    }
+}
