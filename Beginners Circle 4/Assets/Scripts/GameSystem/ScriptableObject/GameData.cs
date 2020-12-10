a281213717a920bb8e555fbd312faefa79162445
YuiAzu <lekcham.kanyanee@gmail.com>
2020-12-10 18:45:37 +0900

    Add sciptable object that store time and score.Also edit pause prefab and timer.

---
+++ Beginners Circle 4/Assets/Scripts/GameSystem/ScriptableObject/GameData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Object/GameData")]
+public class GameData : ScriptableObject
+{
+    [Range(0, 180)]
+    [Tooltip("Time limit per level")]
+    public float[] maxTime;
+    [Tooltip("Max score per level")]
+    public float[] maxScore;
+}
